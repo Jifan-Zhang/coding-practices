bc8d2b0e5fa375ff0c9c5a0c92394012e236f4ba
Jifan Zhang <54615102+Jifan-Zhang@users.noreply.github.com>
2020-02-12 11:08:06 -0600

    Update Medium-Longest Substring Without Repeating Characters.py

--- Medium-Longest Substring Without Repeating Characters.py
+++ Medium-Longest Substring Without Repeating Characters.py
@@ -1,9 +1,10 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if(len(s)==0):
+        length=len(s)
+        if(length==0):
             return 0
         i=0;j=1;l=1
-        while(j!=len(s)):
+        while(j!=length):
             if(s[j] in s[i:j]):
                 l=max(j-i,l)
                 i+=s[i:j].index(s[j])+1
