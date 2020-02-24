ecdc8f35a740fa80fa5f3111d8893d3d21e6d5f7
Jifan Zhang <54615102+Jifan-Zhang@users.noreply.github.com>
2020-02-24 00:23:28 -0600

    Update Hard-(Part)Median of Two Sorted Arrays.c

--- Hard-(Part)Median of Two Sorted Arrays.c
+++ Hard-(Part)Median of Two Sorted Arrays.c
@@ -1,42 +1,79 @@
 #include <stdio.h>
 int target_count=-1;
-int findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size){
+static int original_length;
+float findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size){
+	original_length=nums1Size+nums2Size;
 	if(target_count==-1)
 		target_count=(nums1Size+nums2Size-1)/2; //要干掉target_count个数字
+	printf("一共要干掉%d个\n",target_count);
 	int pos_1_val=*(nums1+target_count/2-1); //val to be compared in a
 	int pos_2_val=*(nums2+target_count/2-1); //val to be compared in b
-	if(target_count<=1){
+	if(target_count==1)
+		pos_1_val=*nums1;
+		pos_2_val=*nums2;
+	/*if(target_count<=1)
+	{
 		if(nums1Size+nums2Size==2)
-			return (float)(nums1+nums2)/2;
+			return (float)(*nums1+*nums2)/2;
 		else if(nums1Size+nums2Size==3)
 			return *nums1>*nums2?*nums1:*nums2;
 		else if(nums1Size+nums2Size==4)
-			//Do something
-			
-	}
-	printf("数组1检测值：%d; 数组2检测值：%d\n",pos_1_val,pos_2_val);
+		{
+			if(*nums1<*nums2){
+				if(*(nums1+nums1Size-1)>*(nums2+nums2Size-1))
+					return nums1Size==2?(float)(*nums2+*(nums2+1))/2:(float)(*nums1+*(nums2+1))/2;
+				else
+					return nums1Size==1?(float)(*nums2+*(nums2+1))/2:(float)(*(nums1+1)+*(nums2))/2;}
+			else{
+				if(*(nums1+nums1Size-1)>*(nums2+nums2Size-1))
+					return nums1Size==2?(float)(*nums1+*(nums2+1))/2:(float)(*nums1+*(nums1+1))/2;
+				else
+					return nums1Size==1?(float)(*nums1+*(nums2+1))/2:(float)(*(nums1+1)+*(nums2))/2;}
+		}
+	}*/
+	printf("数组1(%d)检测值：%d; 数组2(%d)检测值：%d\n",nums1Size,pos_1_val,nums2Size,pos_2_val);
 
 	if(pos_1_val<pos_2_val){
+		if(nums1Size<=target_count/2){
+			printf("数字1清空\n");
+			target_count-=nums1Size;
+			return original_length%2?*nums2:(*nums2+*(nums2+1))/2;
+		}
 		nums1+=target_count/2;
 		nums1Size-=target_count/2;
-		printf("干掉了数组1 小于%d 的数据\n",pos_1_val);
+		printf("干掉了数组1 小于等于%d 的数据\n",pos_1_val);
 	}
 	else{
+		if(nums2Size<=target_count/2){
+			printf("数字2清空\n");
+			target_count-=nums2Size;
+			return original_length%2?*nums1:(*nums1+*(nums1+1))/2;
+		}
 		nums2+=nums2Size/2;
 		nums2Size-=nums2Size/2;
-		printf("干掉了数组2 小于%d 的数据\n",pos_2_val);
+		printf("干掉了数组2 小于等于%d 的数据\n",pos_2_val);
     }
 	target_count-=target_count/2;
-	printf("剩余size：%d\n",target_count);
+	printf("剩余要干掉target_count: %d\n",target_count);
 	if(target_count>1)
 		findMedianSortedArrays(nums1, nums1Size, nums2, nums2Size);
-	//result=((*nums1)-(*nums2))>0 ? (*nums1),(*nums2);
-	printf("数组1开头：%d\n数组2开头：%d\n",*nums1,*nums2);
-	return *nums1>*nums2?*nums1:*nums2;
+	printf("数组1(%d)开头:%d; 数组2(%d)开头:%d\n",nums1Size,*nums1,nums2Size,*nums2);
+	if((original_length)%2)
+		return *nums1>*nums2?*nums1:*nums2;
+	else{
+		if(*nums1>*nums2)
+			return *(nums1+1)>*(nums2+1)?(float)(*(nums2+1)+*nums1)/2:(float)(*nums1+*(nums1+1))/2;
+		else
+			return *(nums1+1)>*(nums2+1)?(float)(*(nums2+1)+*nums2)/2:(float)(*nums2+*(nums1+1))/2;
+			
+		
+	}
+		
 }
 
 int main(){
 	int a[] ={1,2};
-	int b[] ={3,4};
-	printf("中位数：%d",findMedianSortedArrays(a, sizeof(a)/4, b, sizeof(b)/4));	
+	int b[] ={3,4,5,6};
+	printf("中位数：%f",findMedianSortedArrays(a, sizeof(a)/4, b, sizeof(b)/4));	
+	return 0;
 }
