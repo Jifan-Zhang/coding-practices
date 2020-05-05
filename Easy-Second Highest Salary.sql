8e4460ab4a5f1d92f114a785c9915a9c34daa8c0
Jifan Zhang <54615102+Jifan-Zhang@users.noreply.github.com>
2020-05-04 23:50:45 -0500

    Update Easy-Second Highest Salary.sql

--- Easy-Second Highest Salary.sql
+++ Easy-Second Highest Salary.sql
@@ -1,3 +1,6 @@
 select distinct SecondHighestSalary from(
 select nth_value(salary, 2) over() as SecondHighestSalary from( 
 select distinct salary from employee order by salary desc) as t) as t1
+
+#Second way
+#select distinct salary as SecondHighestSalary from employee order by salary desc limit 1 offset 1 
