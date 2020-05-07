a957dd0c9b94f9d699eda94a93dcc3163e53257e
Jifan Zhang <54615102+Jifan-Zhang@users.noreply.github.com>
2020-05-07 14:24:31 -0500

    Add files via upload

---
+++ Medium-New Companies.sql
@@ -0,0 +1,1 @@
+select  company_code,founder,count(distinct lead_manager_code),count(distinct senior_manager_code),count(distinct manager_code),count(distinct employee_code) from company join employee using(company_code) group by company_code,founder
